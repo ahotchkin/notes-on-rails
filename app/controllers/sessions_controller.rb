963b4f06161d79594ee424e58c4adecf0810ca11
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 14:35:43 -0500

    Add methods to create and destroy sessions; add before_action for redirect_if_logged_in before all actions except destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  before_action :redirect_if_logged_in, except: :destroy
+  
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Username or password is incorrect."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
