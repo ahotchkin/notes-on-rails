0ffe3af0ebec2f76ab04e558cd0f3ff881824a3c
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 15:52:24 -0500

    Add edit and update actions and current_note helper method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :redirect_if_not_logged_in
+  helper_method :current_note
 
   def new
     @note = Note.new
@@ -14,10 +15,31 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    if current_user.id == current_note.user.id
+      render :edit
+    else
+      render user_path(current_user)
+    end
+  end
+
+  def update
+    if current_note.update(note_params)
+      redirect_to user_path(current_user)
+      flash[:message] = "Note successfully updated."
+    else
+      render :edit
+    end
+  end
+
   private
 
     def note_params
       params.require(:note).permit(:title, :body)
+    end
+
+    def current_note
+      note = Note.find_by_id(params[:id])
     end
 
 end
