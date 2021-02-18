3b5a53c8d560db5e42677dc8909f21f06bb2d95b
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 12:11:36 -0500

    Run rails g controller Sessions; add root route at sessions#home and corresponding view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  root "sessions#home"
+
   get "/signup", to: "users#new"
 
   # use this for incorrect signup
   post "/signup", to: "users#create"
-  
+
   resources :notes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
