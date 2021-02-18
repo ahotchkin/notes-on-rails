2d9cdb175937d884a9b869e0baf5a9b69647b376
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 12:05:12 -0500

    Add route for signup and actions for get and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get "/signup", to: "users#new"
+
+  # use this for incorrect signup
+  post "/signup", to: "users#create"
+  
   resources :notes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
