44b9c96081a7bd2bef832079d4e821462d15453b
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 15:36:43 -0500

    Add actions for new and create and a private method for note_params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,23 @@
 class NotesController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = current_user.notes.build(note_params)
+    if @note.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+
 end
