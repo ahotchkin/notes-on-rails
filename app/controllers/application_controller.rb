225247f26cb999455bfd4a107c135271ef897d1b
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-18 14:34:16 -0500

    Add methods for current_user, logged_in?, redirect_if_not_logged_in, and redirect_if_logged_in; label current_user and logged_in? as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      redirect_to root_path if !logged_in?
+    end
+
+    def redirect_if_logged_in
+      redirect_to user_path(current_user) if logged_in?
+    end
+
 end
