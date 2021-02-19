7dded8a6d756387fc558c0f91a4021ce5d1fae3d
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-19 14:36:08 -0500

    Add password_confirmation to user_params; update show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,13 +19,23 @@ class UsersController < ApplicationController
     redirect_if_not_logged_in
     # using session[:user_id] so user can't update id in params and see another user's dashboard. If id in url is updated to another id, current_user's dashboard is displayed but manually entered id displays in url.
     @user = User.find_by_id(session[:user_id])
-    redirect_to "/" if !@user
+    
+    if !!@user
+      params[:id] = session[:user_id].to_s
+    end
+    # render user_path(@user)
+    # redirect_if_logged_in
+    # if !!@user
+    #   redirect_if_logged_in
+    # else
+    #   redirect_if_not_logged_in
+    # end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :time_zone, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :time_zone, :password, :password_confirmation)
     end
 
 end
