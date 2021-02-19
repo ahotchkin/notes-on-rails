0849454162e3fb515f0fe791e27918ff15145c81
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-19 11:07:00 -0500

    Add time_zone to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def show
     redirect_if_not_logged_in
-    # using session[:user_id] so user can't update id in params and see another user's dashboard. If id in url is updated to another id, current_user's dashboard is displayed but manually entered id displays in url. 
+    # using session[:user_id] so user can't update id in params and see another user's dashboard. If id in url is updated to another id, current_user's dashboard is displayed but manually entered id displays in url.
     @user = User.find_by_id(session[:user_id])
     redirect_to "/" if !@user
   end
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :time_zone, :password)
     end
 
 end
