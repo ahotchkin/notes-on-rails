13ed1a9806cd14faa678e08788812466db41769f
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-19 14:36:24 -0500

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :notes
+
+  validates :first_name, presence: true
+  validates :email, format: { with: /\A(.+)@(.+)\z/, message: "is invalid"  }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }  
 end
