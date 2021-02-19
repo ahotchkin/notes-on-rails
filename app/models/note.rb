cfaa564dd3d5d1596eb91edb9820fdd81108fd28
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-19 13:09:11 -0500

    Add validations and before_save callback to populate title if it's blank

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, if: -> { body.blank? }
+  validates :title, length: { maximum: 30, too_long: "can't be more than %{count} characters" }
+  validates :body, presence: true, if: -> { title.blank? }
+  validates :body, length: { maximum: 1000, too_long: "can't be more than %{count} characters" }
+
+  before_save do
+    self.title = self.body[0..29] if self.title.blank?
+  end
 end
