5bb5b216bc97d4741152164dc0f7718fea8910e3
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-21 16:21:38 -0500

    Remove space

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :notes
 
   validates :first_name, presence: true
-  validates :email, format: { with: /\A(.+)@(.+)\z/, message: "is invalid"  }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }  
+  validates :email, format: { with: /\A(.+)@(.+)\z/, message: "is invalid"  }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
 end
