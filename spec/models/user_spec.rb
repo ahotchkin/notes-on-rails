b0a1c02f4406daa7c7fd3c14454718f48417cf23
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-21 16:22:02 -0500

    Add tests for validations and associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:janet) {
+    User.create(
+      :first_name => "Janet",
+      :last_name => "Silva",
+      :email => "janet.silva@test.com",
+      :time_zone => "Eastern Time (US & Canada)",
+      :password => "password"
+    )
+  }
+
+  let(:note1) {
+    Note.create(
+      :title => "Janet's first note",
+      :body => "This is the first note that Janet has written.",
+      :user_id => janet.id
+    )
+  }
+
+  it "is valid with a first_name, email, and password" do
+    expect(janet).to be_valid
+  end
+
+  it "is not valid without a first_name" do
+    expect(User.new(:last_name => "Silva", :email => "janet.silva@test.com", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+  end
+
+  it "is not valid without an email" do
+    expect(User.new(:first_name => "Janet", :last_name => "Silva", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+  end
+
+  it "is not valid without a passwod" do
+    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silva@test.com", :time_zone => "Eastern Time (US & Canada)")).not_to be_valid
+  end
+
+  it "has an email in a proper format" do
+    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silva", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "j@.", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silva@test.com", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+  end
+
+  it "has many notes" do
+    expect(janet.notes).to include(note1)
+  end
+
 end
