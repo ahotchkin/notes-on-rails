81824f8932e08e541f02a21ce7e8ed43d8c9c3c8
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-22 13:40:20 -0500

    Update syntax from => to :

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe User, type: :model do
 
   let(:janet) {
     User.create(
-      :first_name => "Janet",
-      :last_name => "Silva",
-      :email => "janet.silva@test.com",
-      :time_zone => "Eastern Time (US & Canada)",
-      :password => "password"
+      first_name: "Janet",
+      last_name: "Silva",
+      email: "janet.silva@test.com",
+      time_zone: "Eastern Time (US & Canada)",
+      password: "password"
     )
   }
 
   let(:note1) {
     Note.create(
-      :title => "Janet's first note",
-      :body => "This is the first note that Janet has written.",
-      :user_id => janet.id
+      title: "Janet's first note",
+      body: "This is the first note that Janet has written.",
+      user_id: janet.id
     )
   }
 
@@ -25,21 +25,21 @@ RSpec.describe User, type: :model do
   end
 
   it "is not valid without a first_name" do
-    expect(User.new(:last_name => "Silva", :email => "janet.silva@test.com", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+    expect(User.new(last_name: "Silva", email: "janet.silva@test.com", time_zone: "Eastern Time (US & Canada)", password: "password")).not_to be_valid
   end
 
   it "is not valid without an email" do
-    expect(User.new(:first_name => "Janet", :last_name => "Silva", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+    expect(User.new(first_name: "Janet", last_name: "Silva", time_zone: "Eastern Time (US & Canada)", password: "password")).not_to be_valid
   end
 
   it "is not valid without a passwod" do
-    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silva@test.com", :time_zone => "Eastern Time (US & Canada)")).not_to be_valid
+    expect(User.new(first_name: "Janet", last_name: "Silva", email: "janet.silva@test.com", time_zone: "Eastern Time (US & Canada)")).not_to be_valid
   end
 
   it "has an email in a proper format" do
-    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silva", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
-    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "j@.", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
-    expect(User.new(:first_name => "Janet", :last_name => "Silva", :email => "janet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silva@test.com", :time_zone => "Eastern Time (US & Canada)", :password => "password")).not_to be_valid
+    expect(User.new(first_name: "Janet", last_name: "Silva", email: "janet.silva", time_zone: "Eastern Time (US & Canada)", password: "password")).not_to be_valid
+    expect(User.new(first_name: "Janet", last_name: "Silva", email: "j@.", time_zone: "Eastern Time (US & Canada)", password: "password")).not_to be_valid
+    expect(User.new(first_name: "Janet", last_name: "Silva", email: "janet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silvajanet.silva@test.com", time_zone: "Eastern Time (US & Canada)", password: "password")).not_to be_valid
   end
 
   it "has many notes" do
