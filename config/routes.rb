41c6dee692ad21d156f260a741244300fe4aa012
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-22 12:20:43 -0500

    Remove routes not being used

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,14 @@ Rails.application.routes.draw do
   root "sessions#home"
 
   get "/signup", to: "users#new"
-
-  # use this for incorrect signup
   post "/signup", to: "users#create"
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  
+
   delete "/logout", to: "sessions#destroy"
 
-  resources :notes
-  resources :users
+  resources :notes, except: :index
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
