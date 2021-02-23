449d9a8d8d2f461ba99125e9a1d62835381a7fdc
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-22 20:57:01 -0500

    Add note_email method

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -1,2 +1,8 @@
 class NoteMailer < ApplicationMailer
+  def note_email(user, note)
+    @user = user
+    @note = note
+
+    mail(to: @user.email, subject: "#{@user.first_name}, You Have a New Note!")
+  end
 end
